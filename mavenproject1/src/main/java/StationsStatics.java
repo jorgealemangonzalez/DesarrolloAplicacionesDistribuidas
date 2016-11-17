f877a92c5b955d76240e6e7ece459aded99ee34f
jorgealemangonzalez <jorge.alemangonzalez@gmail.com>
2016-11-17 08:36:34 +0100

    Station statics primero

--- mavenproject1/src/main/java/StationsStatics.java
+++ mavenproject1/src/main/java/StationsStatics.java
@@ -1,20 +1,34 @@
 
-import java.util.List;
-
-
 /**
  *
  * @author jorgeAleman
  */
-class StationsStatics {
+public class StationsStatics {
     //Objeto con la informacion de las estadisticas que hagamos
-   
+    private int totalFreeSlots ;
+    private int totalOcupiedSlots;
+    private final int totalNumberStations;
+    private final int averageAltitude;
+    
     public StationsStatics(Stations stations) {
-        //TODO crear lsa statics y añadirlas al toString
+        totalFreeSlots = 0;
+        totalOcupiedSlots = 0;
+        totalNumberStations = stations.getStations().size();
+        int cumAltitude = 0;
+        for(Station station : stations.getStations()){
+            totalFreeSlots += Integer.getInteger(station.getSlots());
+            totalOcupiedSlots += Integer.getInteger(station.getBikes());
+            cumAltitude += Integer.getInteger(station.getAltitude());
+        }
+        
+        averageAltitude = cumAltitude / totalNumberStations ;
+        
+    }
+
+    @Override
+    public String toString() {
+        return "StationsStatics{" + "totalFreeSlots=" + totalFreeSlots + ", totalOcupiedSlots=" + totalOcupiedSlots + ", totalNumberStations=" + totalNumberStations + ", averageAltitude=" + averageAltitude + '}';
     }
     
-    @Override
-    public String toString(){
-        return "StationsStatics { " + " } ";
-    }   
+     
 }
